1c9111debce0074969909433c2d87780bdd0edef
Oyibodroid <oyibodaniel247@gmail.com>
2023-11-08 14:57:00 +0100

    filter update

--- js/filter.js
+++ js/filter.js
@@ -1,20 +1,39 @@
-const filterButtons = document.querySelectorAll('.tablinks')
-const filterAbleImgs = document.querySelectorAll('.column img')
+const btn = document.querySelectorAll('.btn')
+const filterImg = document.querySelectorAll('.gallery img')
 
-const filterImg = e => {
-    document.querySelector(".active").classList.remove("active");
-    e.target.classList.add("active");
+const filter = e => {
+    document.querySelector('.active').classList.remove('active');
+    e.target.classList.add('active')
 
-    filterAbleImgs.forEach(Img => {
-        Img.classList.add("hide");
-        if(Img.dataset.name === e.target.dataset.name || e.target.dataset.name === 'all'){
-            Img.classList.remove("hide");
-            console.log('hello world');
+    filterImg.forEach(img => {
+        img.classList.add('hide');
+
+        if (img.dataset.name === e.target.dataset.name ) {
+            img.classList.remove('hide')
+        }else if(img.classList === 'hidden'){
+            img.style.display = 'none'
         }
+
     })
-};
+}
 
 
-filterButtons.forEach(button => button.addEventListener("click", filterImg))
+btn.forEach(btn => {
+    btn.addEventListener('click', filter)
+})
 
-console.log('hello world');
+// animation on scroll
+
+// observer = new IntersectionObserver((entries) => {
+//     entries.forEach(entry => {
+//         if (entry.intersectionRatio > 0) {
+//             entry.target.style.animation = 'slideUp 1s ease'
+//         } else {
+//             entry.target.style.animation = 'none'
+//         }
+//     })
+// })
+
+// filterImg.forEach(img =>{
+//     observer.observe(img)
+// })
